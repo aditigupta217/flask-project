2e731a614ccaa0563b6146b7a908d2c71f7643cd
aditigupta217 <aditigupta221177@gmail.com>
2025-06-08 11:51:37 +0530

    adding login code

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/submit", methods = ["POST"])
+def submit():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    '''if username =="aditi" and password == "pass":
+        return render_template("welcome.html")'''
+    
+    valid_user = {
+        'aditi':'123',
+        'nikita': 'Nikita@432'
+    }
+ 
+    if username in valid_user and password == valid_user[username]:
+        return render_template("welcome.html")
+    else:
+        return "invalid"
